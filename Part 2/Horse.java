b45a00280df61d373bb10f00d3f134e0af4578a7
robotics-hana <163348646+robotics-hana@users.noreply.github.com>
2024-04-26 13:56:20 +0100

    Create Horse.java
    Horse class used in the gui

---
+++ Part 2/Horse.java
@@ -0,0 +1,77 @@
+
+class Horse {
+    private char symbol;
+    private String name;
+    private double confidence;
+    private String breed;
+    private String equipment;
+    private int distanceTravelled;
+    private boolean fallen;
+    private int wins;
+    private int falls;
+
+    public Horse(char symbol, String name, double confidence, String breed, String equipment, int wins, int falls) {
+        this.symbol = symbol;
+        this.name = name;
+        this.confidence = confidence;
+        this.breed = breed;
+        this.equipment = equipment;
+        this.distanceTravelled = 0;
+        this.fallen = false;
+        this.wins = wins;
+        this.falls = falls;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getConfidence() {
+        return confidence;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public String getEquipment() {
+        return equipment;
+    }
+
+    public int getDistanceTravelled() {
+        return distanceTravelled;
+    }
+
+    public boolean hasFallen() {
+        return fallen;
+    }
+
+    public void moveForward() {
+        distanceTravelled++;
+    }
+
+    public void fall() {
+        fallen = true;
+    }
+
+    public void incrementWins() {
+        wins++;
+    }
+
+    public void incrementFalls() {
+        falls++;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public int getFalls() {
+        return falls;
+    }
+
+}
