e76bf3791e714ad3ccaa9b2653666775c3239f90
robotics-hana <163348646+robotics-hana@users.noreply.github.com>
2024-04-26 12:35:11 +0000

    Main method added

--- Part 1/Race.java
+++ Part 1/Race.java
@@ -219,4 +219,24 @@ public class Race
             i = i + 1;
         }
     }
+
+    public static void main(String[] args) {
+        Race race = new Race(20);
+
+        // Create three horses
+        Horse horse1 = new Horse('♘', "PUMPKIN", 0.6);
+        Horse horse2 = new Horse('♞', "BUNNIE", .9);
+        Horse horse3 = new Horse('o', "CUTIEPIE", .9);
+        Horse horse4 = new Horse('o', "CUTIEPIE", .9);
+
+
+        // Add the horses to the race in lanes 1, 2, and 3
+        race.addHorse(horse1, 1);
+        race.addHorse(horse2, 2);
+        race.addHorse(horse3, 3);
+
+        // Start the race
+        race.startRace();
+    }
+
 }
