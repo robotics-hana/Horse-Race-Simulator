c86d86b6108120a8c96483b295b0e7df7baaab68
robotics-hana <163348646+robotics-hana@users.noreply.github.com>
2024-04-26 12:31:42 +0000

    Getters and instance variable initialized

--- Part 1/Horse.java
+++ Part 1/Horse.java
@@ -23,49 +23,65 @@ public class Horse
         }
         // Increment the horseCount to track the number of Horse objects created
         horseCount++;
+
+        // Initialize variables
+        this.distanceTravelled = 0;
+        this.fallen = false;
+        this.name = horseName;
+
+        // Set the symbol, default to '♘' if the given symbol is not valid (not visible)
+        try {
+            isCharacterVisible(horseSymbol);
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+            this.symbol = '♘';
+        }
+
     }
     
     
-    //Other methods of class Horse
-    public void fall()
-    {
-        
-    }
-    
-    public double getConfidence()
-    {
-        
-    }
-    
-    public int getDistanceTravelled()
-    {
-        
-    }
-    
-    public String getName()
-    {
-        
-    }
-    
-    public char getSymbol()
-    {
-        
-    }
-    
-    public void goBackToStart()
-    {
-        
-    }
-    
-    public boolean hasFallen()
-    {
-        
+     // Sets the horse to fallen
+    public void fall() {
+        fallen = true;
     }
 
-    public void moveForward()
-    {
-        
+    
+   // Returns confidence of the horse
+    public double getConfidence() {
+        return confidence;
     }
+    
+    // Returns distance travelled by horse
+    public int getDistanceTravelled() {
+        return distanceTravelled;
+    }
+    
+    // Resets the horse to start position by setting distance travelled to 0
+    public void goBackToStart() {
+        distanceTravelled = 0;
+        fallen = false;
+    }
+
+    // Returns symbol of the horse
+    public char getSymbol() {
+        return symbol;
+    }
+
+    // Returns name of the horse
+    public String getName() {
+        return name;
+    }
+    
+    // Returns true if the horse has fallen
+    public boolean hasFallen() {
+        return fallen;
+    }
+
+    // Increments the distance travelled by the horse
+    public void moveForward() {
+        distanceTravelled++;
+    }
+
 
     public void setConfidence(double newConfidence)
     {
